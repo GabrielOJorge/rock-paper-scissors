d1ff65364193336472c0fc3abf872cde0bcac2c9
GabrielOJorge <gabrieloliveiraj331@gmail.com>
2021-11-17 12:22:02 -0300

    Make game fully functional

--- src/js/main.js
+++ src/js/main.js
@@ -7,28 +7,45 @@ function computerPlay() {
   return randomValue(options);
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    alert("Tie!");
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    alert("You win! Rock beats Scissors.");
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    alert("You lose! Paper beats Rock.");
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    alert("You win! Paper beats Rock.");
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    alert("You lose! Scissors beats Paper.");
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    alert("You win! Scissors beats Paper.");
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    alert("You lose! Rock beats Scissors.");
-  } else if (playerSelection === null) {
-    alert("What are you afraid of?");
-  } else {
-    alert("Choose a possible play!");
-  }
-}
+let playerScore = 0;
+let computerScore = 0;
 
-const playerSelection = prompt("Let the battle begin! Rock, Paper or Scissors?");
-const computerSelection = computerPlay();
-playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+while (playerScore < 3 && computerScore < 3) {
+  function game() {
+    let playerSelection = prompt("Let the battle begin! Rock, Paper or Scissors?");
+    let computerSelection = computerPlay();
+    
+    function playRound(playerSelection, computerSelection) {
+      
+      if (playerSelection === computerSelection) {
+        alert("Tie!");
+      } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        alert("You win! Rock beats Scissors.");
+        playerScore++;
+      } else if (playerSelection === "rock" && computerSelection === "paper") {
+        alert("You lose! Paper beats Rock.");
+        computerScore++;
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        alert("You win! Paper beats Rock.");
+        playerScore++;
+      } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        alert("You lose! Scissors beats Paper.");
+        computerScore++;
+      } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        alert("You win! Scissors beats Paper.");
+        playerScore++;
+      } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        alert("You lose! Rock beats Scissors.");
+        computerScore++;
+      } else if (playerSelection === null) {
+        alert("What are you afraid of?");
+      } else {
+        alert("Choose a possible play!");
+      }
+    }
+    
+    playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+  }
+  
+  game();
+}
