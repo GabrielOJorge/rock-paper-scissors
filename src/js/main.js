524b3a82cd1647b085777877bf0d0ea532c92cbb
GabrielOJorge <gabrieloliveiraj331@gmail.com>
2021-12-07 14:40:58 -0300

    Add feature that starts the game only if the start button is clicked

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const textDisplay = document.querySelector('#text-display')
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const gameChoices = document.querySelector('.game-choices');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -12,35 +13,26 @@ startBtn.addEventListener('click', () => {
   textDisplay.textContent = "Let the battle begin! Rock, Paper or Scissors?";
   startBtn.style.display = 'none';
   game();
+
+  rock.addEventListener('click', () => {
+    playerSelection = 'rock'
+    game();
+  });
+  
+  paper.addEventListener('click', () => {
+    playerSelection = 'paper'
+    game();
+  });
+  
+  scissors.addEventListener('click', () => {
+    playerSelection = 'scissors'
+    game();
+  });
 });
-
-rock.addEventListener('click', () => {
-  playerSelection = 'rock'
-  game();
-});
-
-paper.addEventListener('click', () => {
-  playerSelection = 'paper'
-  game();
-});
-
-scissors.addEventListener('click', () => {
-  playerSelection = 'scissors'
-  game();
-});
-
-
-if (playerScore === 3) {
-  textDisplay.textContent = "Congratulations! You won the game!";
-} else if (computerScore === 3) {
-  textDisplay.textContent = "Sorry, you lost the game!";
-}
-
 
 function computerPlay() {
   const play = ["rock", "paper", "scissors"];
   const randomValue = list => list[Math.floor(Math.random() * list.length)];
-  
   
   return randomValue(play);
 }
@@ -69,12 +61,16 @@ function game() {
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
       textDisplay.textContent = "You lose! Rock beats Scissors.";
       computerScore++;
-    } 
+    }
   }
-
-  console.log(playerSelection);
   
   playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+
+  if (playerScore === 3) {
+    textDisplay.textContent = "Congratulations! You won the game!";
+    gameChoices.style.display = "none";
+  } else if (computerScore === 3) {
+    textDisplay.textContent = "Sorry, you lost the game!";
+    gameChoices.style.display = "none";
+  }
 }
-
-
